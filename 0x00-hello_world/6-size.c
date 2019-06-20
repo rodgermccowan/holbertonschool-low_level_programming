f843d0fa2e7c2dc09e89cc1656b3e085636a05c9
Rodgers McCowan <842@holbertonschool.com>
2019-06-20 04:09:03 +0000

    Print size of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("\nSize of a char: %d byte(s)", sizeof(char *));
+	printf("\nSize of a int: %d byte(s)", sizeof(int *));
+	printf("\nSize of long int: %d byte(s)", sizeof(long int *));
+	printf("\nSize of long long int: %d byte(s)", sizeof(long long int *));
+	printf("\nSize of float: %d byte(s)", sizeof(float y *));
+	return (0);
+}
