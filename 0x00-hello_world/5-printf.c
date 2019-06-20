188fa2747708f19a04b46ad94f3b0abc9865cfd2
Rodgers McCowan <842@holbertonschool.com>
2019-06-20 02:38:00 +0000

    Edit betty header

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - Entry point
+ * main - Entry point
  *
- *Return : 0
+ * Return: 0
  */
-
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
