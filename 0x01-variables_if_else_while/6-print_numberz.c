2fffd5dcdc85fdfd632c560b1255debfb2a42078
Rodgers McCowan <842@holbertonschool.com>
2019-06-21 05:38:12 +0000

    Print numbers 0-9 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 0
+ */
+
+int main(void)
+
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
