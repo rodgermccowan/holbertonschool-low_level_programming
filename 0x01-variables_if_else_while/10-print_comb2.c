56e7dce0c307c8d614009b2b6aeb82c7f46c9334
Rodgers McCowan <842@holbertonschool.com>
2019-06-21 20:25:43 +0000

    Print double digits with space and comma.

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			putchar(x);
+			putchar(y);
+		if (x != '9' || y != '9')
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
