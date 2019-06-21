9ca2538a2ee1fddb60929bfee28007263feb4ca5
Rodgers McCowan <842@holbertonschool.com>
2019-06-21 05:53:03 +0000

    Print the alphabet backwards with putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	int x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
