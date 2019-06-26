154d62f29ce57e2c62dca04ce6a16f17ef068a65
Rodgers McCowan <842@holbertonschool.com>
2019-06-26 00:36:37 +0000

    Update hours to only 2 variable instead of 4

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,24 +10,17 @@ void jack_bauer(void)
 {
 	int a;
 	int b;
-	int c;
-	int d;
 
-	for (a = '0'; a <= '2'; a++)
-	{
-		for (b = '0'; b <= '3'; b++)
+	for (a = 0; a < 24; a++)
+		for (b = 0; b < 60; b++)
 
-			for (c = '0'; c <= '5'; c++)
-
-				     for (d = '0'; d <= '9'; d++)
-
-				{
-					_putchar(a);
-					_putchar(b);
-					_putchar(':');
-					_putchar(c);
-					_putchar(d);
-					_putchar('\n');
-				}
-	}
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(':');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
+		}
 }
+
