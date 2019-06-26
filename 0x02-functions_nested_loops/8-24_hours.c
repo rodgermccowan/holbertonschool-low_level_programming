7c628e308a9d74eb5de383f5fb8f287b7915e67e
Rodgers McCowan <842@holbertonschool.com>
2019-06-26 00:16:29 +0000

    Print the minutes of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints seconds of the entire day
+ *
+ * Return: (void)
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = '0'; a <= '2'; a++)
+	{
+		for (b = '0'; b <= '3'; b++)
+
+			for (c = '0'; c <= '5'; c++)
+
+				     for (d = '0'; d <= '9'; d++)
+
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+	}
+}
